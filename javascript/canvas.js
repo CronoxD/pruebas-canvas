0940a3982a9a9cad2701f657b4bc1418d9e83ae0
Emmanuel Octavio <emmanueloctaviomc@gmail.com>
2018-04-17 19:49:49 -0500

    se agregan las otras esquinas

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -3,14 +3,22 @@ let lienzo = dibujo.getContext('2d');
 
 function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal) {
   lienzo.beginPath();
-  lienzo.strokeStyle = 'red';
+  lienzo.strokeStyle = color;
   lienzo.moveTo(xInicial, yInicial);
   lienzo.lineTo(xFinal,yFinal);
   lienzo.stroke();
   lienzo.closePath();
 }
-var resolucion = 10;
-for(let i=0; i<=500; i+=resolucion) {
-  dibujarLinea('red',0, i, i+resolucion,500);
-  dibujarLinea('red', i, 500, 500, 500-i);
+
+const tamDibujo = 500;
+const resolucion = 10;
+const color = 'blue';
+dibujo.setAttribute("width",tamDibujo+"");
+dibujo.setAttribute("height",tamDibujo+"");
+
+for(let i=0; i<=tamDibujo; i+=resolucion) {
+  dibujarLinea(color,0, i, i+resolucion,tamDibujo);
+  dibujarLinea(color, i, tamDibujo, tamDibujo, tamDibujo-i);
+  dibujarLinea(color, 0, i, tamDibujo-i+resolucion, 0);
+  // dibujarLinea(color, 500-i)
 }
